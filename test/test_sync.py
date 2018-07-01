ca47f87d6c2b2841418dc8750384aed44660f274
Mark Cooper <markchristophercooper@gmail.com>
2018-06-30 19:04:00 -0700

    Mock post requests

--- test/test_sync.py
+++ test/test_sync.py
@@ -1,6 +1,7 @@
 import unittest
 
 from sas.sync import CollectionSpaceSyncEvent
+from unittest.mock import patch
 
 
 class CollectionSpaceSyncEventTestCase(unittest.TestCase):
@@ -13,13 +14,29 @@ class CollectionSpaceSyncEventTestCase(unittest.TestCase):
         'password': 'Administrator',
     }
     sync_url = 'https://core.collectionspace.org/cspace-services/vocabularies/urn:cspace:name(materialuse)/sync'
+    cspace_sync_event = CollectionSpaceSyncEvent(config)
 
     def test_should_return_expected_build_url(self):
-        cspace_sync_event = CollectionSpaceSyncEvent(self.config)
         self.assertEqual(
-            cspace_sync_event.build_url(),
+            self.cspace_sync_event.build_url(),
             self.sync_url
         )
+
+    @patch('sas.sync.requests.post')
+    def test_should_return_post_sync_event_fail(self, mock_post):
+        mock_post.return_value.ok = False
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.text = 'Sad =('
+        self.assertRaises(Exception, self.cspace_sync_event.sync)
+
+    @patch('sas.sync.requests.post')
+    def test_should_return_post_sync_event_success(self, mock_post):
+        mock_post.return_value.ok = True
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.text = 'Happy =)'
+
+        response = self.cspace_sync_event.sync()
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
